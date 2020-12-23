c1e5762edf025a8f2caa669e8e2e617b11b6ba77
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-22 18:36:20 -0600

    Hooked up stories service

--- src/app/services/stories/stories.service.spec.ts
+++ src/app/services/stories/stories.service.spec.ts
@@ -1,20 +1,34 @@
 import { TestBed } from '@angular/core/testing';
+import { TestScheduler } from 'rxjs/testing';
 
 import { StoriesService } from './stories.service';
 
 describe('StoriesService', () => {
   let service: StoriesService;
+  let scheduler: TestScheduler;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StoriesService);
+    scheduler =  new TestScheduler((actual, expected) => {
+      expect(actual).toEqual(expected);
+    });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should define getTopStories', () => {
-    expect(service.getTopStories).toBeDefined();
+  it('should define getTopStories$', () => {
+    expect(service.getTopStories$).toBeDefined();
+  });
+
+  it('should return top 1 story when called with 1', done => {
+    service.getTopStories$(1).subscribe(
+      stories => {
+        expect(stories.length).toEqual(1);
+        done();
+      }
+    );
   });
 });
--- src/app/services/stories/stories.service.ts
+++ src/app/services/stories/stories.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Item } from 'src/app/models/item.type';
+import { Observable, forkJoin } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, retry, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoriesService {
-  getTopStories(): Array<Item> {
-    return [];
+  baseUrl: string;
+
+  constructor() {
+    this.baseUrl = 'https://hacker-news.firebaseio.com/v0';
   }
 
-  constructor() { }
+  private getTopStoryIds$(limitTopNStories: number): Observable<Array<number>> {
+    var url = `${this.baseUrl}/topstories.json?orderBy="$key"&limitToFirst=${limitTopNStories}`;
+    return ajax.getJSON(url)
+      .pipe(
+        retry(2),
+        map(response => response as Array<number>)
+      );
+  }
+
+  private mapItemIdToItem$(itemId: number): Observable<Item> {
+    var url = `${this.baseUrl}/item/${itemId}.json`;
+    return ajax.getJSON(url)
+      .pipe(
+        retry(2),
+        map(response => response as Item)
+      );
+  }
+
+  public getTopStories$(limitTopNStories: number): Observable<Array<Item>> {
+    return this.getTopStoryIds$(limitTopNStories).pipe(
+      switchMap(ids =>
+        forkJoin(
+          ids.map(id => this.mapItemIdToItem$(id))
+        )
+      )
+    );
+  }
 }
