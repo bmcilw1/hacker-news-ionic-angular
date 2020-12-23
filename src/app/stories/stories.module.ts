c12a8bfc59a2f648b6f401447a317cd87935bf9d
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-23 14:16:39 -0600

    Fixed stories module

--- src/app/stories/stories.module.ts
+++ src/app/stories/stories.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoriesComponent } from './stories.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  declarations: [StoriesComponent],
+  exports: [StoriesComponent]
 })
 export class StoriesComponentModule { }
