eb4e878c6446a908fc6e732920e8d4ab3cda0a71
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-24 17:55:29 -0600

    Mock ionic module

--- src/app/view-message/view-message.page.spec.ts
+++ src/app/view-message/view-message.page.spec.ts
@@ -2,9 +2,11 @@ import { ViewMessagePageRoutingModule } from './view-message-routing.module';
 import { ViewMessagePage } from './view-message.page';
 import { MockBuilder, MockRender } from 'ng-mocks';
 import { ActivatedRoute, ActivatedRouteSnapshot, convertToParamMap } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 
 describe('ViewMessagePage', () => {
   beforeEach(() => MockBuilder(ViewMessagePage)
+    .mock(IonicModule)
     .mock(ActivatedRoute, {
       snapshot: { paramMap: convertToParamMap({ id: '1' }) } as ActivatedRouteSnapshot
     }));
