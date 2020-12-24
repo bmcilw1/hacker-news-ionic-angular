ea54d90fb4c3acefd5de1e90cf0b672b3c073010
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-24 16:34:44 -0600

    Fixed memory leak issue

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Item } from '../models/item.type';
 import { StoriesService } from '../services/stories/stories.service';
 
@@ -9,11 +10,16 @@ import { StoriesService } from '../services/stories/stories.service';
 })
 export class StoriesComponent implements OnInit {
   stories: Array<Item>;
+  topStoriesSubscription: Subscription;
 
   constructor(private storiesService: StoriesService) { }
 
   ngOnInit() {
-    this.storiesService.getTopStories$().subscribe(stories => this.stories = stories);
+    this.topStoriesSubscription = this.storiesService.getTopStories$().subscribe(stories => this.stories = stories);
+  }
+
+  ngOnDestroy() {
+    this.topStoriesSubscription.unsubscribe();
   }
 
 }
