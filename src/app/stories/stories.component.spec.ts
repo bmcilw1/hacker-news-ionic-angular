0e7e2eebd871975cfeed5f976d96c1953b5a31de
MG <m@sudo.eu>
2020-12-24 07:43:18 +0100

    tests: format

--- src/app/stories/stories.component.spec.ts
+++ src/app/stories/stories.component.spec.ts
@@ -1,11 +1,12 @@
 import { MockBuilder, MockRender } from 'ng-mocks';
 import { EMPTY } from 'rxjs';
+
 import { StoriesService } from '../services/stories/stories.service';
 
 import { StoriesComponent } from './stories.component';
 import { StoriesComponentModule } from './stories.module';
 
-fdescribe('StoriesComponent', () => {
+describe('StoriesComponent', () => {
   beforeEach(() => MockBuilder(StoriesComponent, StoriesComponentModule)
     .mock(StoriesService, {
       getTopStories$: () => EMPTY,
