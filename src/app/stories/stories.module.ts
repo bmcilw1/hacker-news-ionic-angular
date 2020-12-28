7bc1ba06fbb95cd3ffb8612519f10123a5414e98
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-28 15:39:25 -0600

    Added hyperlinks to external sources

--- src/app/stories/stories.module.ts
+++ src/app/stories/stories.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoriesComponent } from './stories.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   declarations: [StoriesComponent],
   exports: [StoriesComponent]
 })
