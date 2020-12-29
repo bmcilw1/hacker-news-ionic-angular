568372ea8c827b73583aa4b640d3607beaaf6ab3
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-29 14:57:21 -0600

    Prefer before each for reuable setup

--- e2e/integration/home-page.spec.ts
+++ e2e/integration/home-page.spec.ts
@@ -1,10 +1,12 @@
 /// <reference types="Cypress" />
 
 describe('Home Page', () => {
-    it('should display the title on the home page', () => {
+    beforeEach(() => {
         cy.intercept('https://hacker-news.firebaseio.com/v0/topstories.json', { fixture: 'topstories.json' })
         cy.intercept('https://hacker-news.firebaseio.com/v0/item/*', { fixture: 'story.json' })
+    });
 
+    it('should display the title on the home page', () => {
         cy.visit('/');
 
         cy.get('ion-title').contains('Hacker News Ionic Angular')
@@ -12,9 +14,6 @@ describe('Home Page', () => {
     });
 
     it('should display the articles in app-stories as links', () => {
-        cy.intercept('https://hacker-news.firebaseio.com/v0/topstories.json', { fixture: 'topstories.json' })
-        cy.intercept('https://hacker-news.firebaseio.com/v0/item/*', { fixture: 'story.json' })
-
         cy.visit('/');
 
         cy.get('app-stories ion-item a').should('have.length.within', 2, 20).and('be.visible').each(item =>
