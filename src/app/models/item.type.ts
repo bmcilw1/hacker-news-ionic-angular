0c00fc6c18e148338d08fb1d73255ebb06dfc69d
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-29 18:58:43 -0600

    More lint fix

--- src/app/models/item.type.ts
+++ src/app/models/item.type.ts
@@ -1,19 +1,19 @@
 import { Url } from 'url';
 
 export interface Item {
-    id: number,
-    deleted: boolean,
-    type: 'job' | 'story' | 'comment' | 'poll' | 'pollopt',
-    by: string,
-    time: Date,
-    text: HTMLDocument,
-    dead: boolean,
-    parent: number,
-    poll: number,
-    kids: Array<number>,
-    url: Url,
-    score: number,
-    title: HTMLElement,
-    parts: Array<number>,
-    descendants: number
-}
+    id: number;
+    deleted: boolean;
+    type: 'job' | 'story' | 'comment' | 'poll' | 'pollopt';
+    by: string;
+    time: Date;
+    text: HTMLDocument;
+    dead: boolean;
+    parent: number;
+    poll: number;
+    kids: Array<number>;
+    url: Url;
+    score: number;
+    title: HTMLElement;
+    parts: Array<number>;
+    descendants: number;
+};
