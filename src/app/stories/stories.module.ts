839a597d86310e2ab20fd9deca4923f003f8a911
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-30 10:51:34 -0600

    Fixed time ago pipe error

--- src/app/stories/stories.module.ts
+++ src/app/stories/stories.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoriesComponent } from './stories.component';
 import { IonicModule } from '@ionic/angular';
 import { DomainPipe } from '../pipes/domain.pipe';
 import { TimeAgoPipe } from 'time-ago-pipe';
 
+@Pipe({
+  name: 'timeAgo',
+  pure: false
+})
+export class TimeAgoExtendsPipe extends TimeAgoPipe {}
 
 @NgModule({
   imports: [CommonModule, IonicModule],
-  declarations: [StoriesComponent, DomainPipe, TimeAgoPipe],
+  declarations: [StoriesComponent, DomainPipe, TimeAgoExtendsPipe],
   exports: [StoriesComponent]
 })
 export class StoriesComponentModule { }
